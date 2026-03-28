b87f1eddfd645ab9b23e30f69f29217021f77868
Ramiz Arib <ramizarib@hotmail.com>
2026-03-29 02:13:11 +1100

    Fix home page

---
+++ app/page.tsx
@@ -0,0 +1,124 @@
+Skip to content
+
+Find…
+F
+Other
+Overview
+Deployments
+Logs
+Analytics
+Speed Insights
+Observability
+Firewall
+CDN
+Domains
+Integrations
+Storage
+Flags
+Agent
+AI Gateway
+Sandboxes
+Usage
+Settings
+Ship
+26
+Built for you to ship what’s next.
+
+Avatar for ramizarib-3348
+Romis
+
+
+Deployments
+9hueLxhU3
+Deployment
+Logs
+Resources
+Source
+Open Graph
+Deployment Details
+Build Failed
+Command "npm run build" exited with 1
+
+Created
+github/Romiiiis
+Romiiiis
+56s ago
+Status
+Error
+Latest
+Duration
+39s
+Just now
+Environment
+Production
+Domains
+beams-buddies-a6fs-git-main-romis-projects-2bf85688.vercel.app
+beams-buddies-a6fs-pxrw1nxq0-romis-projects-2bf85688.vercel.app
+Source
+main
+53b50d4
+Rename to Jobyra
+
+Deployment Settings
+3 Recommendations
+Build Logs
+39s
+37 lines
+Find in logs
+⌘F
+02:11:16.274 
+> tradelink-crm@0.1.0 build
+02:11:16.275 
+> next build
+02:11:16.275 
+02:11:16.863 
+Attention: Next.js now collects completely anonymous telemetry regarding usage.
+02:11:16.863 
+This information is used to shape Next.js' roadmap and prioritize features.
+02:11:16.864 
+You can learn more, including how to opt-out if you'd not like to participate in this anonymous program, by visiting the following URL:
+02:11:16.864 
+https://nextjs.org/telemetry
+02:11:16.864 
+02:11:16.927 
+   ▲ Next.js 15.2.4
+02:11:16.927 
+02:11:16.942 
+   Creating an optimized production build ...
+02:11:29.697 
+ ✓ Compiled successfully
+02:11:29.699 
+   Linting and checking validity of types ...
+02:11:39.813 
+Failed to compile.
+02:11:39.814 
+02:11:39.814 
+app/page.tsx
+02:11:39.814 
+Type error: File '/vercel/path0/app/page.tsx' is not a module.
+02:11:39.814 
+02:11:39.874 
+Next.js build worker exited with code: 1 and signal: null
+02:11:39.906 
+Error: Command "npm run build" exited with 1
+Deployment Summary
+Deployment Checks
+Assigning Custom Domains
+Runtime Logs
+
+View and debug runtime logs & errors
+
+Observability
+
+Monitor app health & performance
+
+Speed Insights
+
+Not Enabled
+Performance metrics from real users
+
+Web Analytics
+
+Not Enabled
+Analyze visitors & traffic in real-time
+
